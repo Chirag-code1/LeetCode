99afbefa7ad9b84fc6c79b3d8d5eeb46adb03179
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2021-11-30 13:25:00 +0530

    Create #234. Palindrome Linked List using STACK.java

---
+++ #234. Palindrome Linked List using STACK.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        Stack<Integer> st = new Stack<Integer>();
+        ListNode curr = head;
+        boolean isPalin = true;
+        
+        while(curr!=null){
+            st.push(curr.val);
+            curr = curr.next;
+        }
+        curr = head;
+        while(curr!=null){
+            int poppedData = st.pop();
+            if(curr.val == poppedData){
+                isPalin = true;
+            } else {
+                isPalin = false;
+                break;
+            }
+            curr = curr.next;
+        }
+        return isPalin;
+    }
+}
