3d26fb638d2bcd5b090f4ff82f79bd690e8264da
CHIRAG <78530525+Chirag-code1@users.noreply.github.com>
2022-01-13 22:47:32 +0530

    Problem Of Day ( 13.Jan.2022)

--- #452. Minimum Number of Arrows to Burst Balloons.cpp
+++ #452. Minimum Number of Arrows to Burst Balloons.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        if (points.empty()) return 0;
+        if (points.empty()) 
+	return 0;
         sort(points.begin(), points.end(), [] (auto &p1, auto&p2) {
             return p1[1] < p2[1];
         });
